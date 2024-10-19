f81adaef2339bfc94352019baf589001e803bb7d
axelherreram <aherreram22@miumg.edu.gt>
2024-10-18 21:54:03 -0600

    .

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'postgres',  // o el dialecto que estés usando
-  logging: false
+  logging: false,
+  port: process.env.DB_PORT,
 });
 
 module.exports = { sequelize, Sequelize  };  // Exportamos la instancia de sequelize
