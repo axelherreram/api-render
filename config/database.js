92ab2d270e5fe0c9a63735f7d3dfd96719f9b92a
axelherreram <aherreram22@miumg.edu.gt>
2024-10-19 09:43:51 -0600

    .

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: 'postgres',  // o el dialecto que estés usando
+  dialect: 'postgres',  
   logging: false,
   port: process.env.DB_PORT,
 });
--- sql/appLog.js
+++ sql/appLog.js
@@ -6,14 +6,13 @@ async function logActivity(user_id, sede_id, username, action, details) {
       console.error('Undefined or null parameters:', { user_id, action, details });
       return;
     }
-
-    await AppLog.create({
+   /*  await AppLog.create({
       user_id,
       sede_id,  
       username, 
       action,  
       details, 
-    });
+    }); */
     console.log('Activity logged:', { user_id, username, action, details });
   } catch (err) {
     console.error('Error:', err);
